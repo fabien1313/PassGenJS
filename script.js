be74c5677821da39d7eacbc912ea9424ea1c29f6
fabien moreno <fabienmoreno1331@yahoo.com>
2022-11-10 22:06:19 -0600

    arrays

--- script.js
+++ script.js
@@ -11,7 +11,16 @@
 //array for upper
 //array for numbers
 //array for special characters
+
+
+// Grabbing element from HTML
 var generateBtn = document.querySelector("#generate");
+
+// Setting up my arrays to filter into my function/generatePassword
+let lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 's', 'y', 'z'];
+let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'S', 'Y', 'Z'];
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let special = ['~', '`', '!', '@', '#', '$', '%', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '<', '>', ':', ';'];
 
 
 
