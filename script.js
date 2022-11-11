0c58d95a9cb6532999228195a82cf41bd98c6ba2
fabien moreno <fabienmoreno1331@yahoo.com>
2022-11-10 22:04:33 -0600

    Pseudo code

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//MY README BREAKDOWN:
+  
+//Create prompts for password criteria:
+//-Length (at least 8 characters and no more than 128 characters)
+//-character types to include or not (lowercase, uppercase, numeric, and/or special characters)
+//When all prompts are answered then a password should be validated and at least one character type should be selected
+//When all prompts are answered a password is generated that matches the chosen criteria
+//Then the password is either displayed in an alert or written on the page.
+
+//array for lowercase
+//array for upper
+//array for numbers
+//array for special characters
 var generateBtn = document.querySelector("#generate");
 
 
